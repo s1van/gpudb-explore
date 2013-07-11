9ec96750a3f01d153f025af706ab27949cf7a5a5
U-Siyuan-THINK\Siyuan <Siyuan@Siyuan-THINK.(none)>
2013-07-11 12:43:12 -0400

    update query_study.sh

--- analysis/query_study.sh
+++ analysis/query_study.sh
@@ -3,23 +3,34 @@
 
 gmm_usage_plot() {
 	FILES=$1
-	Q1=$2
-	Q2X=$3
-	Q2Y=$4
-	OCSV=$5
+	LEGS=$2
+	OFILE=$3
 	
 	R --slave --vanilla --quiet --no-save << EEE
 	
-	qs <- read.table("$QS")[[1]]
-	q1 <- read.table("$Q1")
-	q2x <- read.table("$Q2X")
-	q2y <- read.table("$Q2Y")
+	require(ggplot2)
+	qfs	<- c($FILES)
+	legs<- c($LEGS)
 	
-	q2sd <- (sweep(1/q2x, MARGIN=2, t(q1), "*") + t(q1)/q2y )
-	colnames(q2sd) <- qs
-	rownames(q2sd) <- qs
-	
-	write.csv(q2sd, "$OCSV")
+	qnum <- length(qfs)
+	if(length(legs) != qnum) {
+		print(qfs);
+		print(legs);
+		stop("the number of the legends does not equal the number of the queries ");
+	}
+
+	data <- {};
+	for(i in 1:qnum) {
+		f <- read.table(qfs[i]);
+		colnames(f) <- c("Time","Bytes");
+		f["Bytes"] <- cumsum(f["Bytes"]);
+		f["Query"] <- legs[i];
+
+		data <- rbind(data, f);
+	}
+#print(data)
+	p <- ggplot(data, aes(Time, Bytes, colour = as.factor(Query), group = as.factor(Query))) + geom_line()
+	ggsave(filename="$OFILE", plot=p)
 	
 EEE
 }
@@ -33,10 +44,17 @@ gmm_usage_parse() {
 
 gmm_batch() {
 	LOGDIR=$1;
+	LEGS=$2;
+	OFILE=$3;
 
 	for file in $(find $LOGDIR -type f); do
 		gmm_usage_parse $file > ${file}.r;
 	done
+
+	FILES=$(find $LOGDIR -regex '.*.r$'| awk '{print "\""$1"\""}'| tr "\n" ","| sed 's/,$//g');
+	gmm_usage_plot "$FILES" "$LEGS" "$OFILE";
+
+	find $LOGDIR -regex '.*.r$'| xargs rm;
 }
 
 ########
