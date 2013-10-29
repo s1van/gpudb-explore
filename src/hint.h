65177de33ec46a27983b8e77d41049b58407655f
Kaibo Wang <wangka@cse.ohio-state.edu>
2013-10-29 14:06:23 -0400

    minor

--- src/hint.h
+++ src/hint.h
@@ -4,9 +4,6 @@
 // Read/write hints
 #define HINT_READ		1
 #define HINT_WRITE		2
-#define HINT_PTRARRAY		4
-#define HINT_PTAREAD		8
-#define HINT_PTAWRITE		16
 #define HINT_DEFAULT	(HINT_READ | HINT_WRITE)
 #define HINT_MASK		(HINT_READ | HINT_WRITE)
 
